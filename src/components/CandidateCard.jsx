923779f2c08813e643248c96082c1284b6094594
botdenoms <dudubot404@gmail.com>
2023-04-24 18:47:41 +0300

    ui: create tab setup without functionality

---
+++ src/components/CandidateCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CandidateCard() {
+  return (
+    <div className='cand-card'>CandidateCard</div>
+  )
+}
--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -1,13 +1,51 @@
+import { useState } from 'react'
+import CandidateCard from './CandidateCard'
+
 const CreateForm = ({close}) => {
+
+    const [candidates, setCandidates] = useState([])
+
+    const addCandidate = ()=>{
+        var temp = candidates
+        temp.push(0)
+        setCandidates([...temp])
+    }
+
     return (
         <div className="create-form">
             <div className="top">
-                <span>New election</span>
+                <span className='span-head'>New election</span>
                 <div className="close" onClick={()=>close()}>
                     x
                 </div>
             </div>
-            
+            <div className="form-rows">
+                <input type="text" name="title" id=""  placeholder="Election title"/>
+            </div>
+            <div className="form-rows">
+                <textarea placeholder="Election Description" cols="30" rows="5"></textarea>
+            </div>
+            <div className="can-box">
+                <div className="cad-list">
+                    {
+                        candidates.length > 0? 
+                        candidates.map((v, i)=> <CandidateCard key={i}/>):
+                        <div className='empty'>
+                           <span>No Candidates present</span> 
+                           <span>Add one below</span> 
+                        </div>
+                    }
+                </div>
+                <div className="add" onClick={()=>addCandidate()}>+</div>
+            </div>
+            <div className="form-rows">
+                <input className="chk-box" type="checkbox"/>
+                <span>multi-cast</span>
+            </div>
+            <div className="form-rows">
+                <input className="chk-box" type="checkbox"/>
+                <span>Anonimous casting</span>
+            </div>
         </div>
     );
 }
