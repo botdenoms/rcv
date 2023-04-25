fa99acaaeddbff2ebfd4e3ca4acdc1469f4c5d85
botdenoms <dudubot404@gmail.com>
2023-04-25 11:58:21 +0300

    ui: candidate card ui implentation with static data

--- src/components/CandidateCard.jsx
+++ src/components/CandidateCard.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
+import reactLogo from '../assets/react.svg'
+
 export default function CandidateCard() {
   return (
-    <div className='cand-card'>CandidateCard</div>
+    <div className='cand-card'>
+      <div className="close cc-card" onClick={()=>alert('remove candidate')}>
+        x
+      </div>
+      <div className='img-box'>
+        <img src={reactLogo} className='cand-pic' alt="candidate" />
+      </div>
+      <div className="cand-info">
+        <span>candidate name</span>
+        <div className="more">
+          <img src={reactLogo} className='party-pic' alt="candidate" />
+          <span>party name</span>
+        </div>
+      </div>
+    </div>
   )
 }
